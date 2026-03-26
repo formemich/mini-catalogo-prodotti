dba92049fd7b11e153e0b0d01df5868d6932ff99
micheleformentini <michele.formentini@gmail.com>
2026-03-26 14:50:33 +0100

    [frontend] -- add blue preset (best for UX)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,16 +6,35 @@ import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primevue/themes';
 import 'primeicons/primeicons.css'
 
 import ToastService from 'primevue/toastservice'
 
 const app = createApp(App)
 
+const auraBluePreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50:  '#eff6ff',
+            100: '#dbeafe',
+            200: '#bfdbfe',
+            300: '#93c5fd',
+            400: '#60a5fa',
+            500: '#3b82f6',
+            600: '#2563eb',
+            700: '#1d4ed8',
+            800: '#1e40af',
+            900: '#1e3a8a',
+            950: '#172554'
+        }
+    }
+});
+
 app.use(router)
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: auraBluePreset,
          options: {
             darkModeSelector: '.p-dark'
         }
